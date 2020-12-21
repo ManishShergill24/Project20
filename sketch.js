db03b75e6568830aadd8212df39955e5f6bdfaaa
ManishShergill24 <74660775+ManishShergill24@users.noreply.github.com>
2020-12-21 10:59:34 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -8,7 +8,7 @@ function setup() {
   car = createSprite(100,200,50,50);
   car.velocityX = speed;
 
-  wall = createSprite(1500,200,60,height/2);
+  wall = createSprite(1500,200,40,height/2);
   wall.shapeColor = (80,80,80);
   
 
